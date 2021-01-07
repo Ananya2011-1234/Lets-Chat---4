6a0de9f9b94a1dca6570dd7a99dcb71c7ef78455
Ananya2011-1234 <75071669+Ananya2011-1234@users.noreply.github.com>
2021-01-07 14:22:26 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,61 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyB2HsgbT5MxIOtaSupQ7jMaUOwDfWPRVNc",
+  authDomain: "kwitter-ca799.firebaseapp.com",
+  projectId: "kwitter-ca799",
+  storageBucket: "kwitter-ca799.appspot.com",
+  messagingSenderId: "494128796051",
+  appId: "1:494128796051:web:a00299ed6e919cf7f51866"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+  Room_names = childKey;
+ //Start code
+
+//ADD YOUR FIREBASE LINKS
+
+user_name = localStorage.getItem("user_name");
+
+document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!";
+
+function addRoom()
+{
+room_name = document.getElementById("room_name").value;
+
+firebase.database().ref("/").child(room_name).update({
+  purpose : "adding room name"
+});
+
+  localStorage.setItem("room_name", room_name);
+  
+  window.location = "kwitter_page.html";
+}
+
+function getData() {  firebase.database().ref("/").on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key;
+     Room_names = childKey;
+     console.log("Room Name - " + Room_names);
+    row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)' >#"+ Room_names +"</div><hr>";
+    document.getElementById("output").innerHTML += row;
+  });
+});
+
+}
+
+getData();
+
+function redirectToRoomName(name)
+{
+console.log(name);
+localStorage.setItem("room_name", name);
+  window.location = "kwitter_page.html";
+}
+
+function logout() {
+localStorage.removeItem("user_name");
+localStorage.removeItem("room_name");
+  window.location = "kwitter.html";
+}
+
+ //End code
+ });});}
+getData();
